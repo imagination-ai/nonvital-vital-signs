883885aea9a05d3c0335865c7016d521a8e2aac1
Osman Baskaya <osmanbaskaya1@gmail.com>
2022-11-05 13:53:58 -0700

    Cors

--- src/signs/main.py
+++ src/signs/main.py
@@ -1,10 +1,12 @@
 import logging
 import os
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
+from starlette.middleware.cors import CORSMiddleware
 
 from signs.config import SignsAppSettings
 from signs.customized_logging import configure_logging
+from signs.middleware import add_middleware
 from signs.routers import notion
 
 configure_logging()
@@ -14,6 +16,22 @@ app = FastAPI()
 v1 = FastAPI()
 
 app_settings = SignsAppSettings()
+add_middleware(app)
+
+origins = ["http://localhost:3000", "https://osmanbaskaya.com"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["DELETE", "GET", "POST", "PUT"],
+    allow_headers=["*"],
+)
+
+
+# @app.exception_handler(Exception)
+# def handle_exception(req, exc):
+#     logger.error("Exception occured", exc_info=exc)  # This does not
+#     return {"Hede": 5}
 
 
 def create_directory(dirs):
@@ -25,16 +43,19 @@ def create_directory(dirs):
             logger.info(f"Skipping creating {directory} since it exists.")
 
 
-@v1.on_event("startup")
+@app.on_event("startup")
 async def startup_event():
     print(app_settings)
 
 
-@v1.get("/", tags=["Index"])
+@app.get("/", tags=["Index"])
 async def index():
     message = "Vital Signs is working! See the docs at /api/v1/docs"
     return {"success": True, "message": message}
 
+
+v1.include_router(notion.router)
+app.mount("/api/v1", v1)
 
 if __name__ == "__main__":
     import uvicorn
@@ -47,7 +68,3 @@ if __name__ == "__main__":
         reload=True,
         workers=1,
     )
-
-
-v1.include_router(notion.router)
-app.mount("/api/v1", v1)
--- src/signs/routers/notion.py
+++ src/signs/routers/notion.py
@@ -1,4 +1,7 @@
+from typing import List
+
 from fastapi import APIRouter
+from pydantic import BaseModel
 from requests import Session
 
 from signs.config import app_settings
@@ -24,24 +27,62 @@ def parse_events_items(results):
     return items
 
 
+class KnowledgePoolResultModel(BaseModel):
+    content: str
+    url: str
+    title: str
+
+    @classmethod
+    def from_result(cls, result):
+        url = _get_url_from_knowledge_pool_result(result)
+        content = _get_content_from_knowledge_pool_result(result)
+        title = _get_title_from_knowledge_pool_result(result)
+        return cls(content=content, title=title, url=url)
+
+
+def _get_url_from_knowledge_pool_result(result):
+    url = ""
+    urls = result["properties"]["url"]["rollup"]["array"]
+    if len(urls) > 0:
+        url = urls[0]["url"]
+
+    return url
+
+
+def _get_content_from_knowledge_pool_result(result):
+    return result["properties"]["Name"]["title"][0]["text"]["content"]
+
+
+def _get_title_from_knowledge_pool_result(result):
+    return result["properties"]["interest-title"]["formula"]["string"]
+
+
 def get_knowledge_pool_highlights(limit):
     url = f"{NOTION_API_BASE_URL}/databases/44f8dcff8df647e8af66847e104e073b/query"
     response = s.post(url, headers=DEFAULT_HEADERS).json()
+
     if "results" in response:
         results = response["results"]
         return [
-            result["properties"]["Name"]["title"][0]["text"]["content"]
+            KnowledgePoolResultModel.from_result(result)
             for result in results[:limit]
         ]
 
 
-@router.get(
-    "/last_highlights",
-    # TODO: Return Type - responses={}
-)
+class NotionResponse(BaseModel):
+    status: int
+    success: bool
+
+
+class KnowledgePoolResponseModel(NotionResponse):
+    data: dict
+
+
+@router.get("/last_highlights", response_model=KnowledgePoolResponseModel)
 async def parse_knowledge_pool(limit: int = 5):
     highlights = get_knowledge_pool_highlights(limit)
-    return {"status": 200, "data": {"highlights": highlights}, "success": True}
+    data = {"highlights": highlights}
+    return KnowledgePoolResponseModel(status=200, success=True, data=data)
 
 
 @router.get(
